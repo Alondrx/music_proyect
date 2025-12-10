60b3a060f5d362b1e204ce1c707ebaddbbc91508
Alondrx <alondrx07@gmail.com>
2025-12-10 10:49:51 -0600

    feat: initial configuration

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+
+const clientId = import.meta.env.VITE_CLIENT_ID
+const clientSecret = import.meta.env.VITE_CLIENT_SECRET
+
+function App() {
+  
+
+  return (
+   <>
+      <h1>Codédex Project Template</h1>
+    </>
+  )
+}
+
+export default App
